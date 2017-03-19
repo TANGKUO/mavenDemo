c813fc3ca58fc673f7c8612fd46f9717dfdfe14e
tangkuo <616507752@qq.com>
2017-03-19 17:36:16 +0800

    优化包路径

---
+++ src/test/java/com/tangkuo/cn/SingletonTest.java
@@ -0,0 +1,11 @@
+package com.tangkuo.cn;
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Company: www.tk.com</p>   
+ * @author   tangkuo
+ * @date    2017年3月19日 下午5:35:40
+ */
+public class SingletonTest {
+	
+}
